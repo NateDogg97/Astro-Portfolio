d62a7664353bb68d7866fb638ce69a91ad9bbb93
NateDogg <natmays97@gmail.com>
2024-03-15 07:43:37 -0500

    pdf still nopt downlaoding

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
-        assetFileNames: "assets/[name].[hash][extname]",
+        assetFileNames: "assets/[name][extname]",
       },
     },
   },
