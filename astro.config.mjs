47ac09fbd6d783347471ddd1f60c5d5f50701a29
NateDogg <natmays97@gmail.com>
2024-03-14 21:54:35 -0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
